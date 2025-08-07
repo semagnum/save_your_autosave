d365ebf71ebb4fd3021b3bbfb5f8dce50694ecec
Spencer Magnusson <semagnum@gmail.com>
2025-08-07 08:00:07 -0600

    Update label and tab name

--- __init__.py
+++ __init__.py
@@ -106,10 +106,10 @@ class OpenFileInEditor(bpy.types.Operator):
 
 class ModalOperatorPanel(bpy.types.Panel):
     bl_idname = 'VIEW3D_PT_modal_ops'
-    bl_label = 'Active Modal Operators'
+    bl_label = 'Save Your Autosave'
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = 'View'
+    bl_category = 'Save Your Autosave'
 
     def _get_matching_autosave_file_timestamp(self, context):
         pid = str(os.getpid())        
